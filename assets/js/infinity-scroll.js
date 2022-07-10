fbe4216adda2b5bd437b41fc06d8e69ebf893852
Peter Ullrich <peter.j.ullrich@gmail.com>
2022-07-10 15:05:38 +0200

    Update infinity scrolling

--- assets/js/infinity-scroll.js
+++ assets/js/infinity-scroll.js
@@ -9,20 +9,28 @@ export default {
 
     return ((scrollTop + clientHeight) / scrollHeight) * 100;
   },
+  scrollEventListener() {
+    const currentScrollPosition = this.scrollPosition();
+
+    const isCloseToBottom =
+      currentScrollPosition > this.threshold &&
+      this.lastScrollPosition <= this.threshold;
+
+    if (isCloseToBottom) this.pushEvent("load-more", {});
+
+    this.lastScrollPosition = currentScrollPosition;
+  },
   mounted() {
     this.threshold = 90;
     this.lastScrollPosition = 0;
 
-    window.addEventListener("scroll", () => {
-      const currentScrollPosition = this.scrollPosition();
-
-      const isCloseToBottom =
-        currentScrollPosition > this.threshold &&
-        this.lastScrollPosition <= this.threshold;
-
-      if (isCloseToBottom) this.pushEvent("load-more", {});
-
-      this.lastScrollPosition = currentScrollPosition;
-    });
+    window.addEventListener("scroll", () => this.scrollEventListener());
+  },
+  destroyed() {
+    // Upon destruction of the table, we need to remove the event listener again.
+    // Otherwise, we might end up with multiple listeners that might fetch data from dead LiveViews.
+    // This change was suggested by `Nicolas Blanco` here: https://github.com/PJUllrich/pragprog-book-tables/issues/1
+    // Thank you very much, Nicolas!
+    window.removeEventListener("scroll", () => this.scrollEventListener());
   },
 };
